b9c671d1974be0a64d70a76f6677e1a02fb6a570
Rafael Elias <rafaelniza2000@hotmail.com>
2022-08-15 23:01:14 -0300

    add animation in Get Started

--- src/lib/components/GetStarted.svelte
+++ src/lib/components/GetStarted.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="flex-center w-[8.75rem] aspect-square bg-blue-gradient rounded-full p-[0.125rem] cursor-pointer"
+  class="container flex-center w-[8.75rem] aspect-square bg-blue-gradient rounded-full p-[0.125rem] cursor-pointer"
 >
   <div class="flex-center flex-col bg-primary w-full h-full rounded-full">
     <div class="flex-start flex-row gap-2">
@@ -17,3 +17,21 @@
     </p>
   </div>
 </div>
+
+<style>
+  .container img {
+    animation: up-down 1.5s ease-in-out infinite;
+  }
+
+  @keyframes up-down {
+    0% {
+      transform: translate(0);
+    }
+    50% {
+      transform: translate(5px, -5px);
+    }
+    100% {
+      transform: translate(0);
+    }
+  }
+</style>
